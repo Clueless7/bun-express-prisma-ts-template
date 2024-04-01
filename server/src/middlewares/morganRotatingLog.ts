d5d2bfda3debae52bb39dbdbce712277ddae511d
Josel Catalan <catalanjosel123@gmail.com>
2024-04-01 11:28:53 +0800

    refactor: make rotating logs more configurable

--- server/src/middlewares/morganRotatingLog.ts
+++ server/src/middlewares/morganRotatingLog.ts
@@ -1,9 +1,12 @@
 import morgan from 'morgan'
 import rfs from 'rotating-file-stream'
 
+const LOG_INTERVAL = '1d' // every 1 day
+const LOG_PATH = 'logs' // in the "logs" folder, this should be made dynamic in production by using path
+
 const accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: 'logs',
+  interval: LOG_INTERVAL,
+  path: LOG_PATH,
 })
 
 export const morganRotatingLog = morgan('combined', { stream: accessLogStream })
